a886c76a688dbfe60667072e4a60ab8689a3f552
Ashraf Azemi <ashraf.chzm@gmail.com>
2021-06-19 14:20:09 +0800

    Layout Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useEffect, useState } from "react";
+import "./App.css";
+
+const urlLauncher = "https://api.spacexdata.com/v4/launches";
+const urlShip = "https://api.spacexdata.com/v4/ships";
+const urlCrew = "https://api.spacexdata.com/v4/crew";
 
 function App() {
+  const [launcher, setLauncher] = useState();
+  const [ship, setShip] = useState();
+  const [crew, setCrew] = useState();
+
+  const fetchLauncher = async () => {
+    const response = await fetch(urlLauncher);
+    const dataLauncher = await response.json();
+    setLauncher(dataLauncher);
+  };
+
+  useEffect(() => {
+    fetchLauncher();
+  }, []);
+
+  console.log(launcher);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav id="header">
+        <section className="title">
+          <h3>Space X</h3>
+        </section>
+        <section className="route">
+          <h3 className="routing">Lauches</h3>
+          <h3 className="routing">Ship</h3>
+          <h3 className="routing">Crew</h3>
+        </section>
+      </nav>
+      <div id="launches"></div>
+      <div id="ship"></div>
+      <div id="crew"></div>
+    </>
   );
 }
 
